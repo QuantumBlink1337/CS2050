7f80e8cb02d69b10a3c656819853232f244ac4ec
Matt Marlow <thequantumblink@gmail.com>
2022-02-03 13:13:20 -0600

    additional lab files

--- prelab2/prelab2.c
+++ prelab2/prelab2.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 
 // Returns the largest float that is less than query. If the query is smaller than the members of the array,
 // then error becomes 1. 
 float findLEQ(const float *array, const float query, int* error, int size) {
-    float largestNumber = 0.0;
+    float largestNumber = 0;
+    // This is necessary to do, otherwise this function's domain is only 0 to infinity. 
+    if (query <= 0) {
+        largestNumber = INT_MIN;
+    }
     for (int i =0; i < size; i++) {
         if (query > array[i] && array[i] > largestNumber) {
             largestNumber = array[i];
@@ -18,14 +23,12 @@ float findLEQ(const float *array, const float query, int* error, int size) {
     if (query > largestNumber) {
         *error = 0;
     }
-        printf("%ld\n", sizeof(array));
 
     return largestNumber;
 }
 int main(void) {
     float array[] = {1.2, 2.4, 3.0, 3.555, -0.5, 10.2, 5000.2};
-    printf("%ld\n", sizeof(array));
     int error = 0;
-    float largestNumber = findLEQ(array, 5000.3, &error, sizeof(array)/sizeof(float));
+    float largestNumber = findLEQ(array, 15, &error, sizeof(array)/sizeof(float));
     printf("%f\n%d\n", largestNumber, error);
 }
---
+++ prelab3/prelab3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float * readFloatFileIntoArray(FILE *fp, int *length); 
+void freePointer(void * ptr);
+
+int main(void) {
+    FILE *fp = NULL;
+    fp = fopen("float.txt", "r");
+    int length = 0;
+    float * floatArray = readFloatFileIntoArray(fp, &length);
+    for (int i = 0; i < length; i++) {
+        printf("%f\n", floatArray[i]);
+    }
+    fclose(fp);
+    freePointer(floatArray);
+
+}
+float * readFloatFileIntoArray(FILE *fp, int *length) {
+    
+    char buffer[50];
+    int size = 0;
+    fgets(buffer, 50, fp);
+    sscanf(buffer, "%d", &size);
+    *length = size;
+    if (size == 0) {
+        return NULL;
+    }
+    float * floatArray = (float*)malloc(sizeof(float) * size);
+    if (floatArray == NULL) {
+        *length = -1;
+        return NULL;
+    }
+    int i = 0;
+    for (; i<size || !feof(fp); i++) {
+        float newFloat = 0.0;
+        fgets(buffer, 50, fp);
+        sscanf(buffer, "%f", &newFloat);
+        floatArray[i] = newFloat;
+    }
+    printf("i: %d\n", i);
+    return floatArray;
+}
+void freePointer(void * ptr) {
+    free(ptr);
+    ptr = NULL;
+}
+
+
