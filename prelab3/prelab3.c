6bb0106eb0c264df2cd4aaf02fddba205ce75b0a
Matt Marlow <thequantumblink@gmail.com>
2022-02-17 13:28:01 -0600

    fixed issues

--- prelab3/prelab3.c
+++ prelab3/prelab3.c
@@ -2,16 +2,17 @@
  #include <stdlib.h>
 
  double * createDoubleArray(const int startingIndex, const int endingIndex, int* error);
- void freeArray(void ** ptr);
+ void freeArray(void * ptr);
 
  int main(void) {
      int error = 0;
      double * array = createDoubleArray(1900, 2000, &error);
-     for (int i = 1900; i <= 2000; i++){
+     for (int i = 1900; i < 2000; i++){
          array[i] = 0+i;
          printf("%f\n", array[i]);
      }
-     freeArray((void**)&array);
+     printf("Ptr: %p\n",array);
+     freeArray(array - 1900);
  }
 double * createDoubleArray(const int sIndex, const int eIndex, int* error) {
     int size = eIndex - sIndex;
@@ -20,15 +21,17 @@ double * createDoubleArray(const int sIndex, const int eIndex, int* error) {
         return NULL;
     }
     double * array = malloc(sizeof(double) * size);
+    printf("Ptr: %p\n", array);
     if (array == NULL) {
         *error = 1;
         return NULL;
     }
     array-= sIndex;
+    printf("Ptr: %p\n", array);
     *error = 0;
     return array;
 
 }
-void freeArray(void ** ptr) {
-    free(*ptr);
+void freeArray(void * ptr) {
+    free(ptr);
 }
